304a4ca4d2f0d550172d5b436bde02c7704f228d
ADITYA <hdbeve1212@gmail.com>
2026-06-09 14:21:33 +0530

    GET REQUESTS WITH API CALL

---
+++ REST_API/rest+AI.py
@@ -0,0 +1,23 @@
+import requests
+from groq import Groq
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client=Groq()
+
+fact_response=requests.get("https://uselessfacts.jsph.pl/api/v2/facts/random",params={"langauge":"en"})
+fact=fact_response.json()["text"]
+print(f"Fact:{fact}")
+
+response=client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{
+        "role":"system","content":"You are a curious Science Explainer"
+
+    },{
+        "role":"user","content":f"Explain this fact in 3 sentences:{fact}"
+    }]
+)
+
+print(f"\nAI: explaination:",response.choices[0].message.content)
