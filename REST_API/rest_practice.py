a404b74e6ad6d6f89010eb6d2998cba480237749
ADITYA <hdbeve1212@gmail.com>
2026-06-09 14:05:13 +0530

    GET REQUEST-FETCH A RANDOM JOKE

--- REST_API/rest_practice.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response=requests.get("https://official-joke-api.appspot.com/random_joke")
-
-print("status:",response.status_code)
-data=response.json()
-
-print("Setup:",data["setup"])
-print("Punchline:",data["punchline"])
